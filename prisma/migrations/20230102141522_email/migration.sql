5576ea22fbdd9ad28c94186dcf5e82e2ddfd684f
Silas Martins <silas.martins2041@gmail.com>
2023-01-02 12:11:30 -0300

    Banco de dados arrumado

--- prisma/migrations/20230102141522_email/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateTable
-CREATE TABLE "Emails" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "email" TEXT NOT NULL
-);
--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -4,29 +4,25 @@ import { prismaClient } from "../../lib/prismaClient";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
 
-  if(method === "POST") {
+  if (method === "POST") {
     const { email } = req.body
 
-    await prismaClient.emails.create({
+    const emails = await prismaClient.emails.create({
       data: {
         email,
       }
     });
 
-    return res.status(201).json({});
+    return res.status(201).json({
+      data: emails,
+    });
     
   } else if (method === "GET") {
-      const { email } = req.query;
+    const emails = await prismaClient.emails.findMany()
 
-      const emails = await prismaClient.emails.findMany({
-        where: {
-          id: {
-            contains: String(email),
-          }
-        }
-      })
-
-    return res.json(emails);
+    return res.status(200).json({
+      data: emails,
+    });
   }
 
   return res.status(404).json({message: 'Route not found.'})
